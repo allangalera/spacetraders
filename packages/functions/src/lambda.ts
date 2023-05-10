d465d7ef82c2fd4224857c0b8fc2f6a5b77d4384
Allan Galera <allangalera@gmail.com>
2023-05-10 15:37:12 -0300

    chore: Update sst and remove unused functions package

--- packages/functions/src/lambda.ts
+++
@@ -1,8 +0,0 @@
-import { ApiHandler } from "sst/node/api";
-import { Time } from "@spacetraders/core/time";
-
-export const handler = ApiHandler(async (_evt) => {
-  return {
-    body: `Hello world. The time is ${Time.now()}`,
-  };
-});
